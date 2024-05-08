09ba2ab63cfaec06f85ed669d1de810f619a008b
Agustin Chavez <agustin.chavez@tiendanube.com>
2024-05-08 11:12:35 -0300

    NestJS with Mongoose and MongoDB

---
+++ src/events/entities/event.entity.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Event extends Document {
+  @Prop()
+  type: string;
+
+  @Prop({ index: true })
+  name: string;
+
+  @Prop(mongoose.SchemaTypes.Mixed)
+  payload: Record<string, any>;
+}
+
+export const EventSchema = SchemaFactory.createForClass(Event);
+EventSchema.index({ name: 1, type: -1 });
